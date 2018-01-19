14242a50fadcf8617f1947df9cca151f1fa94062
Sebastian Peć <sebastianpec@MacBook-Pro-5.local>
2018-01-19 14:26:29 +0100

    magic

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.magic;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/mail")]
+    public class ValuesController : Controller
+    {
+
+        [HttpPost]
+        public void Post([FromBody]Mail mailP)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("kontoszkoleniowedocker@gmail.com");
+            mail.To.Add(mailP.Email);
+            mail.Subject = mailP.Topic;
+            mail.Body = mailP.Message;
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("kontoszkoleniowedocker", "testoweHaslo");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/magic/Class.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.magic
+{
+    public class Mail
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("topic")]
+        public string Topic { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+    }
+}
